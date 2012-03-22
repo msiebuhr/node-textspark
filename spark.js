58da0ba11e605191d1231c0774a90e4c519460f2
Philip Mateescu <github@philipm.us>
2012-03-22 00:13:01 -0500

    Allow spark.js to work either as a node module or as a browser function.

--- spark.js
+++ spark.js
@@ -2,19 +2,28 @@
  * ASCII sparklines.
  */
 
-var ticks = ['▁', '▂', '▃', '▅', '▆', '▇'];
+(function(root) {
+    var ticks = ['▁', '▂', '▃', '▅', '▆', '▇'];
 
-function spark(ints) {
-    var max = Math.max.apply(null, ints),
-        min = Math.min.apply(null, ints);
+	var spark = function (ints) {
+    	var max = Math.max.apply(null, ints),
+			min = Math.min.apply(null, ints);
 
-    var steps = ints.map(function (tick) {
-        var index = Math.round((tick - min) / max * (ticks.length -1));
-        return ticks[index];
-    });
+		var steps = ints.map(function (tick) {
+    		var index = Math.round((tick - min) / max * (ticks.length -1));
+    		return ticks[index];
+		});
 
-    return steps.join("");
+		return steps.join("");
+	}
 
-}
+	if(typeof exports !== 'undefined') {
+		if (typeof module !== 'undefined' && module.exports) {
+			module.exports = spark;
+		}
+		exports.spark = spark;
+	} else {
+		root.spark = spark;
+	}
 
-module.exports = spark;
+})(this || window); 

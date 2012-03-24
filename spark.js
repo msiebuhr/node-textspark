b0b5919be091dd8b8c7fb4051408d06ac278bd76
Philip Mateescu <github@philipm.us>
2012-03-24 05:20:16 -0500

    Replaced tabs with spaces to match original file indentation.

--- spark.js
+++ spark.js
@@ -5,25 +5,25 @@
 (function(root) {
     var ticks = ['▁', '▂', '▃', '▅', '▆', '▇'];
 
-	var spark = function (ints) {
-    	var max = Math.max.apply(null, ints),
-			min = Math.min.apply(null, ints);
+    var spark = function (ints) {
+        var max = Math.max.apply(null, ints),
+            min = Math.min.apply(null, ints);
 
-		var steps = ints.map(function (tick) {
-    		var index = Math.round((tick - min) / max * (ticks.length -1));
-    		return ticks[index];
-		});
+        var steps = ints.map(function (tick) {
+            var index = Math.round((tick - min) / max * (ticks.length -1));
+            return ticks[index];
+        });
 
-		return steps.join("");
-	}
+        return steps.join("");
+    }
 
-	if(typeof exports !== 'undefined') {
-		if (typeof module !== 'undefined' && module.exports) {
-			module.exports = spark;
-		}
-		exports.spark = spark;
-	} else {
-		root.spark = spark;
-	}
+    if(typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            module.exports = spark;
+        }
+        exports.spark = spark;
+    } else {
+        root.spark = spark;
+    }
 
 })(this || window); 
